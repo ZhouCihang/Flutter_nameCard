91e63785c47662067e2144f8d8a1f0712623506b
ZhouCihang <matthewzhou1128@gmail.com>
2020-08-18 13:41:16 +0800

    Able to get user data from JSON

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:core';
 
 void main() => runApp(MyApp());
 
@@ -19,20 +22,47 @@ class MyApp extends StatelessWidget {
 }
 
 class NameCardBasic1 extends StatelessWidget{
+  Future<List<UserData>> _getUsers() async {
+    var data = await http.get("http://www.json-generator.com/api/json/get/bVigOGpCUO?indent=2");
+    var jsonData = json.decode(data.body);
+
+    List<UserData> user = [];
+    for(var u in jsonData){
+      UserData userData = UserData(u["index"], u["name"], u["title"], u["address"], u["tel"]);
+      user.add(userData);
+    }
+    //print("ABC");
+    print(user.length);
+
+    return user;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Align(
         alignment: Alignment.center,
-        child: Container(
-          width: 370,
-          height: 250,
-          color: Colors.lightBlue,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
+        child:Container(
+              child: FutureBuilder(
+                future: _getUsers(),
+                builder: (BuildContext context, AsyncSnapshot snapshot){
+                  if (snapshot.data == null){
+                    return Container(
+                      child: Center(child: Text("Loading..."))
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(snapshot.data[index].name),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+              /*Row(
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(20.0),
@@ -91,9 +121,7 @@ class NameCardBasic1 extends StatelessWidget{
                     child: Icon(Icons.phone_iphone, size: 40,),
                   ),
                 ],
-              ),
-            ],
-          ),
+              ),*/
         ),
       ),
     );
@@ -101,10 +129,11 @@ class NameCardBasic1 extends StatelessWidget{
 }
 
 class UserData{
+  final int index;
   final String name;
   final String title;
   final String address;
   final String tel;
 
-  UserData(this.name, this.title, this.address, this.tel);
+  UserData(this.index, this.name, this.title, this.address, this.tel);
 }
