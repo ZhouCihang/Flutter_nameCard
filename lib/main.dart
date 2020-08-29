e0846ea3bd620ba1976d6a94895563b55709e820
ZhouCihang <matthewzhou1128@gmail.com>
2020-08-29 21:56:36 +0800

    add in onTap() function

--- lib/main.dart
+++ lib/main.dart
@@ -58,45 +58,52 @@ class NameCardBasic1 extends StatelessWidget {
                     scrollDirection: Axis.horizontal,
                     itemCount: snapshot.data.length,
                     itemBuilder: (BuildContext context, int index){
-                      return Container(
-                        margin: EdgeInsets.all(10.0),
-                        width: 200.0,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10.0)),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Padding(
-                                    padding: const EdgeInsets.all(5.0),
-                                    child: Icon(Icons.account_circle, size: 50)
-                                ),
-                                Flexible(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        snapshot.data[index].name,
-                                        style:TextStyle(
-                                        fontSize: 18,
-                                      ),),
-                                      Text(snapshot.data[index].title, style:TextStyle(
-                                        fontSize: 12,
-                                      ),overflow: TextOverflow.fade)
-                                    ],
+                      return GestureDetector(
+                        onTap: (){
+                          Navigator.push(context,
+                            new MaterialPageRoute(builder: (context) => DetailPage(snapshot.data[index]))
+                          );
+                        },
+                        child: Container(
+                          margin: EdgeInsets.all(10.0),
+                          width: 200.0,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: Icon(Icons.account_circle, size: 50)
                                   ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 20),
-                            Text(snapshot.data[index].address),
-                            SizedBox(height: 12),
-                            Text(snapshot.data[index].phone)
-                          ],
+                                  Flexible(
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          snapshot.data[index].name,
+                                          style:TextStyle(
+                                          fontSize: 18,
+                                        ),),
+                                        Text(snapshot.data[index].title, style:TextStyle(
+                                          fontSize: 12,
+                                        ),overflow: TextOverflow.fade)
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 20),
+                              Text(snapshot.data[index].address),
+                              SizedBox(height: 12),
+                              Text(snapshot.data[index].phone)
+                            ],
+                          ),
                         ),
                       );
                       //return Text(snapshot.data[index].name);
@@ -111,6 +118,23 @@ class NameCardBasic1 extends StatelessWidget {
     );
   }
 }
+
+class DetailPage extends StatelessWidget {
+
+  final UserData user;
+
+  DetailPage(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(user.name),
+      ),
+    );
+  }
+}
+
 
 class UserData {
   final int index;
